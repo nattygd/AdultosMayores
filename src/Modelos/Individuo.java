0ec7977984448c0f6232bed434a0f10d8eb8cd26
nattygd <nattystoessel13@gmail.com>
2022-10-24 17:23:44 -0300

    cambio nombre de paquetes

--- src/Modelos/Individuo.java
+++ src/Modelos/Individuo.java
@@ -2,14 +2,11 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package com.mycompany.adultosmayores;
-
+package Modelos;
 /**
  *
  * @author natty
  */
-import java.util.Scanner;
-
 public class Individuo{
     private String Rut;
     private String Nombre;
